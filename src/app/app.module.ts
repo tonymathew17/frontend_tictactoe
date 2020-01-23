9f919cecb0b96978aded3aa6482c84538f13573c
Tony Mathew <tony.b.mathew@accenture.com>
2020-01-23 21:50:05 +0530

    Revert "Feature/dynamic board size (#2)"
    This reverts commit a7365bfc2c9056ef856bce707ecf48b837293aa4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {
-  MatCardModule,
-  MatSelectModule,
-  MatButtonModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatGridListModule
-} from '@angular/material';
+import { MatGridListModule, MatCardModule } from '@angular/material'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { WebSocketService } from './web-socket.service';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [
@@ -25,19 +13,11 @@ import { FormsModule } from '@angular/forms';
     BoardComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     MatGridListModule,
-    MatCardModule,
-    MatSelectModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatFormFieldModule,
-    MatInputModule
+    MatCardModule
   ],
   providers: [WebSocketService],
   bootstrap: [AppComponent]
